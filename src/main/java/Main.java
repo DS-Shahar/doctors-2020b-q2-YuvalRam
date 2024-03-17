20ae98a2662b3c8137b16887620c353a2b202436
YuvalRam <126417248+YuvalRam@users.noreply.github.com>
2024-03-17 09:19:26 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,35 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
+package yuval;
+import java.util.*;
+public class bagrut2020_2 {
+	public static Scanner reader = new Scanner (System.in);
+
+
+	public static void main(String[] args) {
+		Doctor[] doctors = {new Doctor("Reut","Eyes"), new Doctor("Ofri", "Nose")};
+		q_b(doctors);
+		for (int i = 0; i < doctors.length; i++) {
+			System.out.println(doctors[i]);
+		}
+
+	}
+	
+	public static void q_b(Doctor[] doctors) {
+		int c = 0;
+		double sum = 0;
+		double rate = 0;
+		for (int i = 0; i < doctors.length; i++) {
+			System.out.println("please enter rate");
+			rate = reader.nextDouble();
+			while(rate!=-1) {
+				sum += rate;
+				c++;
+				System.out.println("please enter rate");
+				rate = reader.nextDouble();
+			}
+			doctors[i].setRate(sum/c);
+			c = 0;
+			sum = 0;
+		}
+	}
+
 }
